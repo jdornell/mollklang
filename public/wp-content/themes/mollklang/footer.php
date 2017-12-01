96606f3df67e92d5b31b423aeeadcb149b35907f
Johan Dornell <johan@Johans-MacBook-Air.local>
2017-12-01 14:24:21 +0100

    Remove default wp content from footer

--- public/wp-content/themes/mollklang/footer.php
+++ public/wp-content/themes/mollklang/footer.php
@@ -1,10 +1,9 @@
 </div> <!-- /.container -->
 
-		<footer class="blog-footer">
-      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-      <p>
-        <a href="#">Back to top</a>
-      </p>
+		<footer id="footer">
+        <div class="footer-content">
+            <p>&copy; Mollklang 2017</p>
+        </div>
     </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
