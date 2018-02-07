2e184fe5bced41cc7a3dc30438b698a32562314d
Johan Dornell <johan@johans-air.mynet>
2018-02-07 15:26:56 +0100

    Remove blog content

--- public/wp-content/themes/mollklang/content.php
+++ public/wp-content/themes/mollklang/content.php
@@ -1,3 +1,5 @@
+<!--
+    BLOG CONTENT
 <div class="blog-post">
 <h2 class="blog-post-title"><?php the_title(); ?></h2>
 <p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
@@ -5,3 +7,5 @@
 <?php the_content(); ?>
 
 </div><!-- /.blog-post -->
+
+Hej
